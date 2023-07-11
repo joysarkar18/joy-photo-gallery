4d6d1419bfb67c39604fe0d1d9262672e6744e7d
Joy Sarkar <joysarkar8171@gmail.com>
2023-07-11 23:59:40 +0530

    random page added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const client = createClient('ZSjOnt4sEf56eVz9tcFPA1yXDQ4RubPeAdrVfeCowG2trYS2vFE
 
 function App() {
   const [photoList , setPhotos] = useState([]);
-  const [pageNo , setPageNo] = useState(Math.floor(Math.random() * 600));
+  const [pageNo , setPageNo] = useState(Math.floor(Math.random() * 700));
   const [sPage , setSpage] = useState(1);
   const [query , setQuery] = useState("");
   const [loading , setLoading] = useState(true);
